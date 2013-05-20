aeb451c29685f0a4f2709be589943cf5a24404a6
Juan S. Castano <jscastanoc@gmail.com>
2013-05-20 09:40:56 -0500

    Added comments and documentation to nip_fuzzy_sources function

--- nip_fuzzy_sources.m
+++ nip_fuzzy_sources.m
@@ -1,9 +1,27 @@
 function aff = nip_fuzzy_sources(cortex, sigma)
+% aff = nip_fuzzy_sources(cortex, sigma)
+% This function returns a matrix that contains information about the
+% distance between points in a graph. It places a gaussian with variance = sigma
+% in each vertex
+% Input:
+%       cortex -> Struct. Structure containing the vertices and faces of
+%       the graph
+%       sigma -> Scalar. Variance of the gaussian placed at each vertex
+% Output:
+%       aff -> NdxNd. Symmetrical matrix in which the i-th column
+%       represents the gaussian placed a round the i-th vertex.
+%
+% Additional comments: This function uses the graph toolbox to compute the
+% distance between each vertex.
 %
 % Juan S. Castaño C.
 % 14 Mar 2013
+
 Nd = num2str(size(cortex.vertices,1));
 
+% Search for a file with the precompute geodesic distances. If not found,
+% computes them and saves them in a file (This WILL take a while, grab a
+% snickers).
 file_name = strcat(fileparts(which('nip_init')),'/data/','dist_mat',num2str(Nd),'.mat');
 if exist(file_name,'file')
     load(file_name)
