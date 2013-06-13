f8e6a124bc7f938298e736b1e28ea83907c7edb5
Juan S. Castano <jscastanoc@gmail.com>
2013-06-13 15:09:41 -0500

    Added documentation to nip_sflex

--- nip_sflex.m
+++ nip_sflex.m
@@ -1,6 +1,19 @@
 function [J_est, extras] = nip_sflex(y, L, basis, reg_par)
 %  [J_est, extras] = nip_sflex(y, L, basis)
+% Implements "Large-scale EEG/MEG source localization with spatial
+% flexibility." by Haufe et al 2011
 %
+% Input:
+%       y -> NcxNt. Matrix containing the data,
+%       L -> NcxNd. Lead Field matrix
+%       basis-> NdxNs. Matrix containing the spatial basis functions.
+%       reg_par-> Scalar. Regularization parameter (1e-6 by default)
+% Output:
+%       J_rec -> NdxNt. Reconstructed activity (solution)
+%       extras.regpar -> Scalar. Optimum regularization parameter
+%
+% Additional comments: Uses the DAL optimization toolbox.
+% 
 % Juan S. Castano C.
 % 13 June 2013
 
