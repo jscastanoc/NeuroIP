1bb81b26836b00b1cfe47b3d409356c62026e8b5
jscastanoc <jscastanoc@gmail.com>
2013-08-20 11:42:35 +0200

    PERFOMANCE, delete useless calculations in nip_neightbor_mat

--- nip_neighbor_mat.m
+++ nip_neighbor_mat.m
@@ -13,30 +13,21 @@ function [Laplacian, QG] = nip_neighbor_mat(cortex)
 %       QG          -> Aproximation of the green function.
 % 
 % Additional Comments
-% The computation of the Green function is based on a script written by:
-% Jose David Lopez - Gareth Barnes - Vladimir Litvak 
 %
 % Juan S. Castano C.
 % jscastanoc@gmail.com
 % 27 Jan 2013
-
-Nd = size(cortex.vertices,1);
-s    = 0.6;
-A     = triangulation2adjacency(cortex.faces);
-GL    = A - spdiags(sum(A,2),0,Nd,Nd);
-GL    = GL*s/2;				% Smoother
-Qi    = speye(Nd,Nd);
-QG    = sparse(Nd,Nd);
-for i = 1:8				% Taylor series approximation
-    QG = QG + Qi;
-    Qi = Qi*GL/i;
+if ~isfield(cortex, 'vc') && ~isfield(cortex,'tri')
+    cortex.vc = cortex.vertices;
+    cortex.vertices = [];
+    cortex.tri = cortex.faces;
+    cortex.faces = [];
 end
-QG    = QG.*(QG > exp(-10));		% Eliminate small values
-QG    = QG*QG;				% Guarantee positive semidefinite matrix
-% clear Qi A GL
+
+Nd = size(cortex.vc,1);
 
 options.normalize = 1;
-G = compute_mesh_gradient(cortex.vertices,cortex.faces,'distance',options);
+G = compute_mesh_gradient(cortex.vc,cortex.tri,'distance',options);
 Laplacian = sparse(G'*G);
 Laplacian = Laplacian + 0.7*abs(min(min(Laplacian)))*eye(size(Laplacian,1));
 Laplacian = Laplacian/max(max(Laplacian));
